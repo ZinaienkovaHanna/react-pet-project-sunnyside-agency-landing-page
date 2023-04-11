b12d9328c54fdfe18336770ecc46233299e3f35a
Hanna <zinaienkova.anna@gmail.com>
2023-04-11 07:53:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/Header/Header';
+import Services from './components/Services/Services';
+import Testimonials from './components/Testimonials/Testimonials';
+import Footer from './components/Footer/Footer';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Services />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
