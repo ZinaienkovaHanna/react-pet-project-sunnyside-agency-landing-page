7d8b0ccd7ab5a2493b1bdec3285247221c2cffcb
Hanna <zinaienkova.anna@gmail.com>
2023-04-13 12:13:38 +0300

    add Services code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import { useState } from 'react';
 
 import styles from './Header.module.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function handleMenuClick() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+  return (
+    <header className={styles.headerBg}>
+      <div className={styles.headerContainer}>
+        <img src="/images/logo.svg" alt="Logo" />
+
+        <nav
+          className={`${styles.headerNav} ${
+            isMenuOpen ? styles.headerNavOpen : ''
+          }`}
+        >
+          <ul className={styles.headerMenu}>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+
+        <button
+          className={`${styles.buttonBurger} ${
+            isMenuOpen ? styles.buttonBurgerActive : ''
+          }`}
+          onClick={handleMenuClick}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+      <h1>We are creatives</h1>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,7 +1,52 @@
 import styles from './Services.module.css';
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className={styles.gridContainer}>
+      <img
+        src="/images/mobile/image-transform.jpg"
+        alt="Transform"
+        className={styles.transformImg}
+      />
+      <div className={styles.transformText}>
+        <h2>Transform your brand</h2>
+        <p>
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you.
+        </p>
+        <a href="#about">LEARN MORE</a>
+      </div>
+      <img
+        src="/images/mobile/image-stand-out.jpg"
+        alt="Stand-out"
+        className={styles.standOutImg}
+      />
+      <div className={styles.standOutText}>
+        <h2>Stand out to the right audience</h2>
+        <p>
+          Using a collaborative formula of designers, researchers,
+          photographers, videographers, and copywriters, we’ll build and extend
+          your brand in digital places.
+        </p>
+        <a href="#about">LEARN MORE</a>
+      </div>
+      <div className={styles.graphicDesign}>
+        <h3>Graphic Design</h3>
+        <p>
+          Great design makes you memorable. We deliver artwork that underscores
+          your brand message and captures potential clients’ attention.
+        </p>
+      </div>
+      <div className={styles.photography}>
+        <h3>Photography</h3>
+        <p>
+          Increase your credibility by getting the most stunning, high-quality
+          photos that improve your business image.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
